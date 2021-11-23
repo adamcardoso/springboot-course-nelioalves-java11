7b805e06ca0404597df62ab96bfb8c93fb8243cc
Adam Cardoso <adamjuniorcardoso@gmail.com>
2021-11-23 20:53:39 -0300

    creating project and User entity

---
+++ src/main/java/com/example/springproject_nelioalves/SpringProjectNelioAlvesApplication.java
@@ -0,0 +1,12 @@
+package com.example.springproject_nelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectNelioAlvesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectNelioAlvesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/springproject_nelioalves/SpringProjectNelioAlvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springproject_nelioalves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectNelioAlvesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
