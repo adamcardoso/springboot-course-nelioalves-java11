5de829cdd84c166b86c6aec47328de672ce8d290
Adam Cardoso <adamjuniorcardoso@gmail.com>
2021-11-30 19:55:39 -0300

    OrderItem, many-to-many association with extra attributes Part 3

--- src/main/java/com/example/springproject_nelioalves/entities/pk/OrderItemPK.java
+++ src/main/java/com/example/springproject_nelioalves/entities/pk/OrderItemPK.java
@@ -7,16 +7,15 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
-import java.util.Objects;
 
 @Embeddable
 public class OrderItemPK implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
+
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
@@ -24,29 +23,44 @@ public class OrderItemPK implements Serializable {
     public Order getOrder() {
         return order;
     }
-
     public void setOrder(Order order) {
         this.order = order;
     }
-
     public Product getProduct() {
         return product;
     }
-
     public void setProduct(Product product) {
         this.product = product;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof OrderItemPK)) return false;
-        OrderItemPK that = (OrderItemPK) o;
-        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((order == null) ? 0 : order.hashCode());
+        result = prime * result + ((product == null) ? 0 : product.hashCode());
+        return result;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(order, product);
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderItemPK other = (OrderItemPK) obj;
+        if (order == null) {
+            if (other.order != null)
+                return false;
+        } else if (!order.equals(other.order))
+            return false;
+        if (product == null) {
+            if (other.product != null)
+                return false;
+        } else if (!product.equals(other.product))
+            return false;
+        return true;
     }
 }
