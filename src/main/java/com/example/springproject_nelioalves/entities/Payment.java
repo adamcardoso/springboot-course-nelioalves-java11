fe81dc7d2cba542ec852068ef8bfbbc75ac7e057
Adam Cardoso <adamjuniorcardoso@gmail.com>
2021-12-01 19:48:27 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/example/springproject_nelioalves/entities/Payment.java
@@ -0,0 +1,68 @@
+package com.example.springproject_nelioalves.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private Instant Date;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant date, Order order) {
+        Id = id;
+        Date = date;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Instant getDate() {
+        return Date;
+    }
+
+    public void setDate(Instant date) {
+        Date = date;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Payment)) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(Id, payment.Id) && Objects.equals(Date, payment.Date) && Objects.equals(order, payment.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, Date, order);
+    }
+}
